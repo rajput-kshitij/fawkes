a17b6a40f211b5f57d93070f5a620df9d98e2574
Rajput <you@example.com>
2020-12-06 20:13:59 +0530

    fix merge conflict

--- fawkes/cli/cli.py
+++ fawkes/cli/cli.py
@@ -113,7 +113,7 @@ if __name__ == "__main__":
         text_match_trainer.generate_keyword_weights(fawkes_config_file)
     elif action == FawkesActions.TRAIN_LSTM_MODEL:
         import fawkes.algorithms.categorisation.lstm.trainer as lstm_trainer
-        lstm_trainer.train_lstm_model(app_config_file)
+        lstm_trainer.train_lstm_model(fawkes_config_file)
     elif action == FawkesActions.SUMMARIZE:
         import fawkes.algorithms.summarization.summarization as summarize
         summarize.generate_summary(app_config_file)
@@ -122,4 +122,3 @@ if __name__ == "__main__":
         similarity.get_similar_reviews_for_app(app_config_file, query_term, 20)
     else:
         raise Exception("Invalid action!")
-        
